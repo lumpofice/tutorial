1295cd67d7bcada7025e440b1dbc4fae2c582309
Jonathan Parker <jparker007@gmail.com>
2023-07-02 08:24:00 -0400

    adding input-within-the-index-view-to-output-within-the-integer_input-view functionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,12 +7,25 @@ from app.main.forms import IntegerInputForm
 @bp.route('/', methods=['GET', 'POST'])
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
-    my_cmd = ['app/templates/index']
+
+    index_view = ['app/templates/index']
     with open('app/templates/index.html', 'w') as outfile:
-        subprocess.run(my_cmd, stdout=outfile)
+        subprocess.run(index_view, stdout=outfile)
+
     form = IntegerInputForm()
     if form.validate_on_submit():
-        flash ('Integer was {}'.format(form.integer.data))
-        return redirect(url_for('main.index'))
+
+        integer = form.integer.data
+        
+        integer_input_view = open("app/templates/integer_input.html", "w")
+        subprocess.call(['bash', \
+                './app/templates/./integer_input', \
+                str(integer)], stdout=integer_input_view)
+
+        return redirect(url_for('main.integer_input'))
     return render_template('index.html', title='Tutorials', form=form)
 
+@bp.route('/result', methods=['GET', 'POST'])
+def integer_input():
+
+    return render_template('integer_input.html', title='Tutorials')
