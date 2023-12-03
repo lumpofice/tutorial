c23d9fcbb2f8764f20dd187cec540d24485fabeb
Jonathan Parker <jparker007@gmail.com>
2023-12-03 11:32:11 -0500

    adding an in order traversal of a binary tree to the agarwal chapter 6 file

--- DSA/agarwal/agarwal_ch_6.py
+++ DSA/agarwal/agarwal_ch_6.py
@@ -37,3 +37,46 @@ while current:
     print(current.data)
     current = current.left_child
 
+
+# Binary tree in-order traverse
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+n0 = Node("root")
+
+n1 = Node("left")
+n0.left_child = n1
+
+n2 = Node("right")
+n0.right_child = n2
+
+n3 = Node("left_0")
+n1.left_child = n3
+
+n4 = Node("left_1")
+n1.right_child = n4
+
+n5 = Node("right_0")
+n2.left_child = n5
+
+n6 = Node("right_1")
+n2.right_child = n6
+
+n7 = Node("left_0_1")
+n3.right_child = n7
+
+print("")
+print("Let's work with in-order traversals of Binary Trees")
+print("")
+
+def in_order_traverse(node):
+    current = node
+    if current is None:
+        return
+    print(current.data)
+    in_order_traverse(current.left_child)
+    in_order_traverse(current.right_child)
+in_order_traverse(n0)
