70e81b80eab4d5b76ee0afc6549e61207eca6625
Jonathan Parker <jparker007@gmail.com>
2023-12-06 09:39:29 -0500

    making a better program that can generate the second element of an orthonormal basis

--- LA/axler/axler_chapter_6.jl
+++ LA/axler/axler_chapter_6.jl
@@ -2,12 +2,14 @@
 using LinearAlgebra
 using VMLS
 
-x = rand(1:50, 2, 1)
-y = rand(1:50, 2, 1)
+x = [1, 2, 3, -4]
+y = [-5, 4, 3, 2]
 
+# So, orthonormal basis will have size OB = 2
+OB = 2
+
+# Orthonormal Basis: set_e
 set_e = Set([])
-println("set_e empty:")
-println(set_e)
 
 println("x:")
 println(x)
@@ -22,16 +24,24 @@ push!(set_e, e)
 println("set_e after element 1 added:")
 println(set_e)
 
-term = 0
-
-for i = 1:size(x, 1)
-	global term
-	term += y[i]*collect(set_e)[1][i]
+for k = 1:(OB - 1)
+	global y
+	global set_e
+	new_element_terms = Set([])	
+	for j = 1:k
+		term_dot = 0
+		for i = 1:size(x, 1)
+			term_dot += y[i]*collect(set_e)[j][i]
+		end
+		push!(new_element_terms, term_dot.*collect(set_e)[j])
+	end
+	summing_non_y_terms = zeros(size(y, 1))
+	for n = 1:length(new_element_terms)
+		summing_non_y_terms -= collect(new_element_terms)[n]
+	end
+	new_element = y + summing_non_y_terms
+	push!(set_e, (1/norm(new_element))*new_element)
+	println("set_e after element 2 added:")
+	println(set_e)
 end
 
-e = zeros(0)
-append!(e, y - term.*collect(set_e)[1])
-push!(set_e, e)
-
-println("set_e after element 2 added:")
-println(set_e)
