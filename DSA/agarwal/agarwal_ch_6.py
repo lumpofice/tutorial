76a2d7a9d42d120959380754c2493146fdf3c8b4
Jonathan Parker <jparker007@gmail.com>
2023-12-05 21:20:03 -0500

    adding a post order tree traversal to the agarwal chapter 6 file

--- DSA/agarwal/agarwal_ch_6.py
+++ DSA/agarwal/agarwal_ch_6.py
@@ -46,28 +46,28 @@ class Node_pre_order:
         self.left_child = None
         self.right_child = None
 
-n0 = Node_pre_order("root")
+n0_pre = Node_pre_order("root")
 
-n1 = Node_pre_order("left")
-n0.left_child = n1
+n1_pre = Node_pre_order("left")
+n0_pre.left_child = n1_pre
 
-n2 = Node_pre_order("right")
-n0.right_child = n2
+n2_pre = Node_pre_order("right")
+n0_pre.right_child = n2_pre
 
-n3 = Node_pre_order("left_0")
-n1.left_child = n3
+n3_pre = Node_pre_order("left_0")
+n1_pre.left_child = n3_pre
 
-n4 = Node_pre_order("left_1")
-n1.right_child = n4
+n4_pre = Node_pre_order("left_1")
+n1_pre.right_child = n4_pre
 
-n5 = Node_pre_order("right_0")
-n2.left_child = n5
+n5_pre = Node_pre_order("right_0")
+n2_pre.left_child = n5_pre
 
-n6 = Node_pre_order("right_1")
-n2.right_child = n6
+n6_pre = Node_pre_order("right_1")
+n2_pre.right_child = n6_pre
 
-n7 = Node_pre_order("left_0_1")
-n3.right_child = n7
+n7_pre = Node_pre_order("left_0_1")
+n3_pre.right_child = n7_pre
 
 print("Printing pre-order nodes:")
 
@@ -78,7 +78,7 @@ def pre_order_traverse(node):
     print(current.data)
     pre_order_traverse(current.left_child)
     pre_order_traverse(current.right_child)
-pre_order_traverse(n0)
+pre_order_traverse(n0_pre)
 
 print("")
 
@@ -89,28 +89,28 @@ class Node_in_order:
         self.left_child = None
         self.right_child = None
 
-n0 = Node_in_order("root")
+n0_in = Node_in_order("root")
 
-n1 = Node_in_order("left")
-n0.left_child = n1
+n1_in = Node_in_order("left")
+n0_in.left_child = n1_in
 
-n2 = Node_in_order("right")
-n0.right_child = n2
+n2_in = Node_in_order("right")
+n0_in.right_child = n2_in
 
-n3 = Node_in_order("left_0")
-n1.left_child = n3
+n3_in = Node_in_order("left_0")
+n1_in.left_child = n3_in
 
-n4 = Node_in_order("left_1")
-n1.right_child = n4
+n4_in = Node_in_order("left_1")
+n1_in.right_child = n4_in
 
-n5 = Node_in_order("right_0")
-n2.left_child = n5
+n5_in = Node_in_order("right_0")
+n2_in.left_child = n5_in
 
-n6 = Node_in_order("right_0_0")
-n5.left_child = n6
+n6_in = Node_in_order("right_0_0")
+n5_in.left_child = n6_in
 
-n7 = Node_in_order("right_0_1")
-n5.right_child = n7
+n7_in = Node_in_order("right_0_1")
+n5_in.right_child = n7_in
 
 print("Printing in-order nodes:")
 
@@ -121,6 +121,53 @@ def in_order_traverse(node):
     in_order_traverse(current.left_child)
     print(current.data)
     in_order_traverse(current.right_child)
-in_order_traverse(n0)
+in_order_traverse(n0_in)
+
+print("")
+
+
+# Binary tree post-order
+class Node_post_order:
+    def __init__(self, data):
+        self.data = data
+        self.left_child = None
+        self.right_child = None
+
+n0_post = Node_post_order("root")
+
+n1_post = Node_post_order("left")
+n0_post.left_child = n1_post
+
+n2_post = Node_post_order("right")
+n0_post.right_child = n2_post
+
+n3_post = Node_post_order("left_0")
+n1_post.left_child = n3_post
+
+n4_post = Node_post_order("left_1")
+n1_post.right_child = n4_post
+
+n5_post = Node_post_order("left_1_0")
+n4_post.left_child = n5_post
+
+n6_post = Node_post_order("left_1_1")
+n4_post.right_child = n6_post
+
+n7_post = Node_post_order("right_0")
+n2_post.left_child = n7_post
+
+n8_post = Node_post_order("right_1")
+n2_post.right_child = n8_post
+
+print("Printint post-order nodes:")
+
+def post_order_traverse(node):
+    current = node
+    if current is None:
+        return
+    post_order_traverse(current.left_child)
+    post_order_traverse(current.right_child)
+    print(current.data)
+post_order_traverse(n0_post)
 
 print("")
