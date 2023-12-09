01b52e7a2fc96ad53c8fdfe249e401df05fc7062
Jonathan Parker <jparker007@gmail.com>
2023-12-09 08:47:20 -0500

    altering the javascript orthonormal basis program to produce an orthonormal basis for four standard basis vectors, adding comments to both the julia and js orthonormal basis programs

--- LA/axler/axler_chapter_6.jl
+++ LA/axler/axler_chapter_6.jl
@@ -37,6 +37,7 @@ for k = 2:dimension
 	global ortho_basis
 	new_element_terms = Set([])	
 	for j = 1:k-1
+		# Building each <e, y>e in this looping
 		term_dot = 0
 		for i = 1:size(x_1, 1)
 			term_dot += 
@@ -44,10 +45,12 @@ for k = 2:dimension
 		end
 		push!(new_element_terms, term_dot.*collect(ortho_basis)[j])
 	end
+	# Summing each <e, y>e term within new_element_terms
 	summing_scaled_terms = zeros(size(x_1, 1))
 	for n = 1:length(new_element_terms)
 		summing_scaled_terms -= collect(new_element_terms)[n]
 	end
+	# This is y - <e1, y>e1 - ... - <em, y>em in Gram-Schmidt
 	new_element_before_scaling = collect(basis)[k] + summing_scaled_terms
 	push!(ortho_basis, 
 	       (1/norm(new_element_before_scaling)).*new_element_before_scaling)
@@ -60,4 +63,3 @@ println("Turning Set to Array:")
 ortho_basis_array = [i for i in ortho_basis]
 typeof(ortho_basis_array)
 println(ortho_basis_array)
-
--- LA/axler/axler_chapter_6.js
+++ LA/axler/axler_chapter_6.js
@@ -1,77 +1,85 @@
 // Orthonormal Basis of two dimensions
 
-var x = [1, 2, 3, -4];
-var y = [-5, 4, 3, 2];
+var x_1 = [1, 0, 0, 0];
+var x_2 = [0, 1, 0, 0];
+var x_3 = [0, 0, 1, 0];
+var x_4 = [0, 0, 0, 1];
 
-const OB = 2;
+var basis = [];
+basis.push(x_1);
+basis.push(x_2);
+basis.push(x_3);
+basis.push(x_4);
 
-var set_e = new Set([]);
+const dimension = 4;
 
-console.log("Here is x:");
-console.log(x);
-
-console.log("Here is y:");
-console.log(y);
+var ortho_basis = new Set([]);
 
 var e = [];
 
 var the_sum = 0;
-for (var i = 0; i < x.length; i++) {
-	the_sum += (x[i])**2;
+for (var i = 0; i < x_1.length; i++) {
+	the_sum += (x_1[i])**2;
 }
-var norm_x = Math.sqrt(the_sum);
-
-for (var i = 0; i < x.length; i++) {
-	var scaled_x_coordinate = (1/norm_x)*x[i];
-	e.push(scaled_x_coordinate);
+var norm_x_1 = Math.sqrt(the_sum);
+for (var i = 0; i < x_1.length; i++) {
+	var scaled_x_1_coordinate = (1/norm_x_1)*x_1[i];
+	e.push(scaled_x_1_coordinate);
 }
 
-console.log("Here is e_1:");
-console.log(e);
+ortho_basis.add(e);
 
-set_e.add(e);
+console.log("Here is ortho_basis containing e_1:");
+console.log(ortho_basis);
+console.log("");
 
-console.log("Here is set_e containing e_1:");
-console.log(set_e);
-
-for (var k = 0; k < OB-1; k++) {
+for (var k = 2; k < dimension+1; k++) {
 	var e = [];
 	var new_element_terms = new Set([]);
-	for (j = 0; j <= k; j++) {
+	for (j = 0; j <= k-2; j++) {
+		// Building each <e, y>e in this loop
 		var term = [];
 		var term_dot = 0;
-		for (i = 0; i < x.length; i++) {
-			term_dot += y[i]*Array.from(set_e)[j][i];
+		for (i = 0; i < x_1.length; i++) {
+			term_dot += basis[k-1][i]*Array.from(ortho_basis)[j][i];
 		}
-		for (var n = 0; n < x.length; n++) {
+		for (var n = 0; n < x_1.length; n++) {
 			var scaled_term_coordinate = 
-				(term_dot)*Array.from(set_e)[j][n];
+				(term_dot)*Array.from(ortho_basis)[j][n];
 			term.push(scaled_term_coordinate);
 		}
 		new_element_terms.add(term);
 	}
-	var summing_non_y_terms = Array.apply(null, Array(4)).
+	// Summing each <e, y>e term within new_element_terms
+	var summing_scaled_terms = Array.apply(null, Array(4)).
 		map(function (x, i) {return 0;});
 	for (var m = 0; m < new_element_terms.size; m++) {
-		summing_non_y_terms = 
-			summing_non_y_terms.map(
-				(x, index) => x - 
-				Array.from(new_element_terms)[m][index]
-			);	
+		summing_scaled_terms = 
+			summing_scaled_terms.map((x, index) => x - Array.from(new_element_terms)[m][index]);	
 	}
-	var new_element_before_scaling = y.map(function(x, index) {
-		return x + summing_non_y_terms[index];
+	// This is the y - <e1, y>e1 - ... - <em, y>em numerator in the
+	// Gram-Schmidt Process
+	var new_element_before_scaling = basis[k-1].map(function(x, index) {
+		return x + summing_scaled_terms[index];
 	});
+	// This is the norm in the denominator of the Gram-Schmidt Process
 	var the_sum = 0;
 	for (a = 0; a < new_element_before_scaling.length; a++) {
 		the_sum += (new_element_before_scaling[a])**2;
 	}
 	var norm_new_element_before_scaling = Math.sqrt(the_sum);
+	// This loop applies the norm to each term in the 
+	// new_element_before_scaling
 	for (b = 0; b < new_element_before_scaling.length; b++) {
 		e.push((1/norm_new_element_before_scaling)*
 		new_element_before_scaling[b]);
 	}
-	set_e.add(e);
-	console.log("set_e after element 2 added:");
-	console.log(set_e);
+	ortho_basis.add(e);
+	console.log("ortho_basis after new element added:");
+	console.log(ortho_basis);
+	console.log("");
 }
+
+console.log("Turning Set to Array:");
+var ortho_basis_array = Array.from(ortho_basis);
+console.log(ortho_basis_array);
