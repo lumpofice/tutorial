8ed63c3e4994a707082bdd79c12d97d619f719ef
Jonathan Parker <jparker007@gmail.com>
2023-12-17 09:45:20 -0500

    adding the vector a version of chebyshev, the equivalent of the vector a version of Chebyshev found in the boyd chapter 3 chebyshev julia file, to the boyd chapter 3 chebyshev js file

--- LA/boyd/boyd_3_chebyshev.jl
+++ LA/boyd/boyd_3_chebyshev.jl
@@ -10,37 +10,37 @@ _______________________________________
 =# 
 
 n = 50;
-x = rand(1:n, 40, 1);
+x_scalar = rand(1:n, 40, 1);
 count = 0;
-a = stdev(x);
+count_stdev = 0;
+a_scalar = stdev(x_scalar);
 
-for i = 1:size(x, 1)
-	if abs(x[i] - avg(x)) > a
+for i = 1:size(x_scalar, 1)
+	if abs(x_scalar[i]) > a_scalar
 		global count += 1;
+	end
+	if abs(x_scalar[i] - avg(x_scalar)) > a_scalar
+		global count_stdev += 1;
 	end
 end
 
-println("Total coordinates above ", a, ":");
-println(count);
+norm_x_scalar = norm(x_scalar);
+println("Norm is ", norm_x_scalar)
 println();
-
-norm_x = norm(x);
-println("Norm is ", norm_x)
-println();
-rms_x = norm(x)/sqrt(size(x, 1));
-rhs_cheby = (rms_x/a)^2;
-lhs_cheby = count/size(x, 1);
+rms_x_scalar = norm(x_scalar)/sqrt(size(x_scalar, 1));
+rhs_cheby = (rms_x_scalar/a_scalar)^2;
+lhs_cheby = count/size(x_scalar, 1);
 
 println("Here is Chebyshev");
 println("LHS: ", lhs_cheby);
 println("RHS: ", rhs_cheby);
 println()
 
-std_x = stdev(x);
-println("Stadard Deviation is ", std_x, ":");
+std_x_scalar = stdev(x_scalar);
+println("Stadard Deviation is ", std_x_scalar, ":");
 println();
-rhs_cheby_std = (std_x/a)^2;
-lhs_cheby_std = count/size(x, 1);
+rhs_cheby_std = (std_x_scalar/a_scalar)^2;
+lhs_cheby_std = count_stdev/size(x_scalar, 1);
 
 println("Here is Chebyshev for standard deviation");
 println("LHS: ", lhs_cheby_std);
--- LA/boyd/boyd_3_chebyshev.js
+++ LA/boyd/boyd_3_chebyshev.js
@@ -2,7 +2,7 @@
 console.log("Here is Chebyshev with a fixed a varaible.");
 console.log("");
 var n = 50;
-var x = Array.from({length: 40}, () => Math.floor(Math.random()*50));
+var x = Array.from({length: 40}, () => Math.floor(Math.random()*n));
 
 // Finding the average of the x coordinates
 var sum_x_terms = 0;
@@ -28,8 +28,8 @@ var stdev_x_square_terms_sum = 0;
 for (var i = 0; i < x.length; i++) {
 	stdev_x_square_terms_sum += stdev_x_square_terms[i];
 }
-var stdev_x = Math.sqrt(stdev_x_square_terms_sum/x.length);
 
+var stdev_x = Math.sqrt(stdev_x_square_terms_sum/x.length);
 console.log("stdev_x:");
 console.log(stdev_x);
 console.log("");
@@ -38,7 +38,7 @@ console.log("");
 // larger than the stdev_x
 var count = 0;
 for (var i = 0; i < x.length; i++) {
-	if (demean_x[i] > stdev_x) {
+	if (Math.abs(demean_x[i]) > stdev_x) {
 		count += 1;
 	}
 }
@@ -51,3 +51,76 @@ if (count/x.length < (stdev_x/stdev_x)**2) {
 	console.log("Chebyshev works when a = stdev_x");
 }
 
+
+// Chebyshev on each coordinate of a random vector
+console.log("");
+console.log("Chebyshev with a varaibles contained within a vector.");
+var a = Array.from({length : 40}, () => Math.ceil(Math.random()*n));
+var x_vector = Array.from({length: 40}, () => Math.floor(Math.random()*n));
+
+// Computing the mean of vector x
+var sum_x_vector_terms = 0;
+for (var i = 0; i < x_vector.length; i++) {
+	sum_x_vector_terms += x_vector[i];
+}
+
+var mean_x_vector = sum_x_vector_terms/x_vector.length
+
+// Demeaning vector x
+var demean_x_vector = Array.apply(null, Array(0)).map(function () {});
+for (var i = 0; i < x_vector.length; i++) {
+	demean_x_vector.push(x_vector[i] - mean_x_vector);
+}
+
+// Computing the standard deviation of x
+var x_vector_stdev_square_terms = 
+	Array.apply(null, Array(0)).map(function () {});
+for (var i = 0; i < x_vector.length; i++) {
+	x_vector_stdev_square_terms.push(demean_x_vector[i]**2);
+}
+
+var x_vector_stdev_square_terms_sum = 0;
+for (var i = 0; i < x_vector.length; i++){
+	x_vector_stdev_square_terms_sum += x_vector_stdev_square_terms[i];
+}
+
+var stdev_x_vector = 
+	Math.sqrt(x_vector_stdev_square_terms_sum/x_vector.length);
+console.log("");
+console.log("stdev_x_vector");
+console.log(stdev_x_vector);
+console.log("");
+
+// Iterating over vector a and comparing LHS with RHS of Chebyshev for 
+// each coordinate of vector a
+for (var i = 0; i < a.length; i++){
+	var count_vector = 0;
+	for (var j = 0; j < x_vector.length; j++){
+		if (Math.abs(demean_x_vector[j]) > a[i]){
+			count_vector += 1;
+		}
+	}
+	console.log("---------------------");
+	console.log("-------a is:---------");
+	console.log(a[i]);
+	console.log("-------count is:-----");
+	console.log(count_vector);
+	console.log("---stdev is:---------");
+	console.log(stdev_x_vector);
+	console.log("---------------------");
+	if ((count_vector/x_vector.length) >= (stdev_x_vector/a[i])**2){
+		console.log("Chebyshev did not hold");
+		console.log("LHS:");
+		console.log(count_vector/x_vector.length);
+		console.log("RHS:");
+		console.log((stdev_x_vector/a[i])**2);
+		console.log("----------------");
+	}
+	else {	
+		console.log("LHS:");
+		console.log(count_vector/x_vector.length);
+		console.log("RHS:");
+		console.log((stdev_x_vector/a[i])**2);
+		console.log("----------------");
+	}
+}
