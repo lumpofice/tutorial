69d786cb14e28d307246d0b610a519390fcb0553
Jonathan Parker <jparker007@gmail.com>
2023-12-31 12:15:14 -0500

    working on the fractional part of decimal conversion to binary

--- DSA/patt/patt_chapter_2.jl
+++ DSA/patt/patt_chapter_2.jl
@@ -1,12 +1,14 @@
 # Converting decimal to binary
-for n = -141:141
+for n = 2:.5:4
 	num = n
 	arr = []
-	function g(h)
+	arr_frac = []
+	function g(h, t)
 		if isempty(arr)
 			return 0
 		else
-			return join(string.(reverse(arr)))
+			arr_string = join(string.(reverse(arr)))
+			return arr_string*t
 		end
 	end
 	function f_1(x, arr)
@@ -70,11 +72,33 @@ for n = -141:141
 			return j(f_2(abs(x)-1, arr))
 		end
 	end
+	function t(d, arr_frac)
+		i = 1
+		while d > 0
+			while d < (2.0)^(-i)
+				append!(arr_frac, 0)
+				i += 1
+			end
+			append!(arr_frac, 1)
+			d = d - (2.0)^(-i)
+			i += 1
+		end
+		arr_frac = vcat(["."], arr_frac)
+		arr_frac_string = join(string.(arr_frac))
+		return arr_frac_string
+	end
 	function k(num, arr)
+		parts = modf(num)
+		integer_part = floor(Int, parts[2])
+		fractional_part = parts[1]
 		if num == -1
 			println(11)
 		else
-			println(g(h(num, arr)))
+			println(g(
+				h(integer_part, arr), 
+				t(abs(fractional_part), arr_frac)
+				)
+			)
 		end
 	end
 	k(num, arr)
