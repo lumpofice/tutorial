897c62cd03245a1c5bde0989614b29ad1b790425
Jonathan Parker <jparker007@gmail.com>
2024-01-13 15:34:37 -0500

    making progress with the javascript version of the decimal to binary conversion program

--- DSA/patt/patt_chapter_2.js
+++ DSA/patt/patt_chapter_2.js
@@ -1,6 +1,6 @@
 // Converting decimal to binary
 
-for (var n = -2; n <= 2; n += 0.5) {
+for (var n = -5; n <= 2; n += 0.5) {
 	var decimal = n;
 	var binary_array_integer = [];
 	var binary_array_fraction = [];
@@ -26,6 +26,99 @@ for (var n = -2; n <= 2; n += 0.5) {
 			return arr_string.concat('', fractional);
 		}
 		// more code here
+	}
+
+	function nonnegative(integer_part, binary_array_integer) {
+		if (integer_part == 0) {
+			return;
+		}
+		for (var i = 0; i < integer_part; i++) {
+			if (integer_part >= 2**(i)) {
+				continue;
+			}
+			else {
+				if (binary_array_integer.length == 0) {
+					for (var j = 0; j < i+1; j++) {
+						binary_array_integer.push(0);
+					}
+				}
+				nonnegative(
+					integer_part - 2**(i-1),
+					binary_array_integer
+				);
+				binary_array_integer[i] = 1;
+				return binary_array_integer;
+			}
+		}
+	}
+
+	function negative(x, binary_array_integer) {
+		if (x == 0) {
+			return
+		}
+		for (var i = 1; i <= x; i++) {
+			if (x >= 2**(i)) {
+				continue
+			}
+			else {
+				if (binary_array_integer.length == 0) {
+					var index_1_to_x = [];
+					for (var j = 1; j < x+1; j++) {
+						index_1_to_x.push(j);
+					}
+					var powers_of_2_minus_1 = 
+						index_1_to_x.map(
+							y => 2**(y) - 1);
+					if (powers_of_2_minus_1.includes(x)) {
+						for (
+							var k = 1;
+							k <= i+2; 
+							k++
+						) {
+							binary_array_integer.
+								push(0);
+						}
+					}
+					else {
+						for (
+							var k = 1;
+							k <= i+1;
+							k++
+						) {
+							binary_array_integer.
+								push(0);
+						}
+					}
+				}
+				negative(x - 2**(i-1), binary_array_integer);
+				binary_array_integer[i-1] = 1
+				return
+			}
+		}
+	}
+
+	function rearranging_the_negative(negative) {
+		for (var i = 0; i < binary_array_integer.length; i++) {
+			if (binary_array_integer[i] == 0) {
+				binary_array_integer[i] = 1;
+			}
+			else {
+				binary_array_integer[i] = 0;
+			}
+		}
+		return binary_array_integer;
+	}
+
+	function signature(integer_part, binary_arry_integer) {
+		if (integer_part >= 0) {
+			return nonnegative(integer_part, binary_array_integer);
+		}
+		else {
+			return rearranging_the_negative(
+				negative(Math.abs(integer_part)-1,
+					binary_array_integer)
+			);
+		}
 	}
 
 	function fractional(d, binary_array_fraction) {
@@ -63,6 +156,17 @@ for (var n = -2; n <= 2; n += 0.5) {
 		if (fractional_part_process_0 == -1) {
 			if (decimal == -1) {
 				console.log("11");
+			}
+			else {
+				console.log(
+					final_outputs(
+						signature(
+							integer_part, 
+							binary_array_integer
+						),
+						""
+					)
+				);
 			}
 		}
 		else {
